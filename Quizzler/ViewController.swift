ab34f1606cdfeeb1cbe03ae0e1b27aa503d89021
omerokumus <omerokumus3@gmail.com>
2023-03-30 16:40:40 +0300

    implementation is removed. Only design remains

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Ömer Faruk Okumuş on 6.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+}
+
